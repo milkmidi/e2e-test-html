d35073112ffcc14f2b3f01c7756ca04d06d3ca05
milkmidi <milkmidi.liao@positivegrid.com>
2022-01-03 14:43:50 +0800

    feat: add main yml

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   base: './',
   plugins: [vue()],
-  build: {
-    outDir: 'docs',
-  },
+  // build: {
+  //   // outDir: 'docs',
+  // },
 });
