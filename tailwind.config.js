02410288be3fd591c3d0719cc475a54bac923a93
milkmidi <milkmidi.liao@positivegrid.com>
2022-01-21 13:42:44 +0800

    feat: upgrade package version

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
